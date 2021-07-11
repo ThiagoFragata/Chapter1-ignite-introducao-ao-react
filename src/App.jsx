e9125e876ab9873911e10dce1510b376011d7940
thiago henrique fragata <thiago.henrique.fragata@hotmail.com>
2021-07-11 13:33:48 -0400

    adicionando react-refresh no webpack

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import { Counter } from "./components/Counter";
 import { RepositoryList } from "./components/RepositoryList";
-import "./styles/globals.scss"
+import "./styles/globals.scss";
 
 export function App() {
-
   return (
-    <RepositoryList />
+    <>
+      <RepositoryList />
+      <br />
+      <br />
+      <br />
+      <br />
+      <Counter />
+    </>
   );
 }
