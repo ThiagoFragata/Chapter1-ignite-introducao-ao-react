80fe21501dfd9b3362660dab652ce6870f5c43ed
thiago henrique fragata <thiago.henrique.fragata@hotmail.com>
2021-07-10 20:01:56 -0400

    first commit

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,21 @@
+export function RepositoryList() {
+    return (
+        <section className="reposiroty-list">
+            <h1>Lista de repositórios</h1>
+            <ul>
+                <li>
+                    <strong>unform</strong>
+                    <p>Forms em react.js</p>
+
+                    <a href="">Acessar repositório</a>
+                </li>
+                <li>
+                    <strong>unform</strong>
+                    <p>Forms em react.js</p>
+
+                    <a href="">Acessar repositório</a>
+                </li>
+            </ul>
+        </section>
+    );
+}
