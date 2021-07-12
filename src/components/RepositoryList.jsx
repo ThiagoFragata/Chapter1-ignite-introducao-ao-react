7e391e6944fc6503d9f2645d47ba963526e9223d
thiago henrique fragata <thiago.henrique.fragata@hotmail.com>
2021-07-12 14:41:45 -0400

    listando os repositorio

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -3,32 +3,22 @@ import { useEffect, useState } from "react";
 
 import "../styles/repositories.scss";
 
-
-// https://api.github.com/users/ThiagoFragata/repos;
-
-const repository = {
-  name: "unform",
-  description: "Forms in React.js",
-  link: "https://github.com/unform/unform",
-};
-
 export function RepositoryList() {
   const [repositories, setRepositories] = useState([]);
 
   useEffect(() => {
     fetch("https://api.github.com/users/ThiagoFragata/repos")
-    .then(response => response.json())
-    .then(data => setRepositories(data))
+      .then((response) => response.json())
+      .then((data) => setRepositories(data));
   }, []);
 
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
       <ul>
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
+        {repositories.map(repository => {
+          return <RepositoryItem key={repository.name} repository={repository} />;
+        })}
       </ul>
     </section>
   );
